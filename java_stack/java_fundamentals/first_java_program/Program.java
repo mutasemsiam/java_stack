2f6efc40d624366a7d515688e4950ed49368a94d
Mutasem Siam <mutasemsiam4@gmail.com>
2022-10-24 13:13:16 +0300

    initial

---
+++ java_stack/java_fundamentals/first_java_program/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("My name is Mutasem Siam");
+		System.out.println("I'm 200 years old");
+		System.out.println("My hometown is Palestine");
+
+	}
+}
