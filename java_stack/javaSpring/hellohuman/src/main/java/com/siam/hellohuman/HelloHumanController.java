6ca1f816a943e832b356fd40f65f5db568d37c9f
Mutasem Siam <mutasemsiam4@gmail.com>
2022-10-31 21:57:10 +0200

    some edit

--- java_stack/javaSpring/hellohuman/src/main/java/com/siam/hellohuman/HelloHumanController.java
+++ java_stack/javaSpring/hellohuman/src/main/java/com/siam/hellohuman/HelloHumanController.java
@@ -11,22 +11,40 @@ public class HelloHumanController {
 	public String hello(@RequestParam(value="name", required=false) String searchQuery1, @RequestParam(value="times", required=false) String searchQuery3,
 			@RequestParam(value="last_name", required=false) String searchQuery2) {
 		// TODO Auto-generated method stub
-		if (searchQuery1 == null) {
+		if (searchQuery1 == null && searchQuery2 ==null && searchQuery3==null) {
 		return "Hello Human";
 		}
 		
 		else if (searchQuery2 ==null && searchQuery3==null) {
 			return "Hello " + searchQuery1;
 		}
+
+		else if (searchQuery1==null && searchQuery3==null) {
+			return "Hello " + searchQuery2; 
+		}
+
 		else if (searchQuery3 ==null) {
 			return "Hello " + searchQuery1 + " " + searchQuery2;
 			
 		}
+		else if(searchQuery1==null && searchQuery2==null) {
+			String str = "Hello ";
+			String repeat = str.repeat(Integer.parseInt(searchQuery3));
+			
+			return repeat;
 		
-		
+		}
 		else if (searchQuery2 ==null) {
 			
 			String str = "Hello " + searchQuery1  + " ";
+			String repeat = str.repeat(Integer.parseInt(searchQuery3));
+			
+			return repeat;
+		}
+		
+		else if(searchQuery1==null){
+			
+			String str = "Hello "  + " " + searchQuery2 + " ";
 			String repeat = str.repeat(Integer.parseInt(searchQuery3));
 			
 			return repeat;
